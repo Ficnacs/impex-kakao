9e80dc7eb3380192a0edfcf4484245c860b74898
Ficnacs <99893141+Ficnacs@users.noreply.github.com>
2022-02-18 01:10:08 -0800

    Add files via upload

---
+++ admin/about/impvideos.php
@@ -0,0 +1,35 @@
+<?php
+<!DOCTYPE html>
+<html>
+<body style="text-alighn: center;">
+     <h1 style="color: green;">
+           IMPEX KAKAO SOLUTIONS
+          </h1>
+
+    <h2>
+  VIDEOS SECTIONS
+    </h2>
+    <p>Cocoa</p>
+    <video width = "500" height="1000px"
+        controls = "controls"/>
+        <source src="C:\X\htdocs/company_website/admin/about/vid/cocoa1.mp4"
+           type="mp4">
+    </video>
+    <br/>
+    <h3> VIDEO NUMBER TWO </h3>
+    <p> video 2 </p>
+
+    <video width = "500px" height="500px"
+        controls = "controls"/>
+        <source src="C:\X\htdocs/company_website/coc.mp4"
+           type="video/mp4">
+    </video>
+        <br/>
+    <img src = "C:\X\htdocs/company_website/admin/about/vid/page0001.jpg" alt="page0001"
+        style="width: 1000px;height:1000px;">
+        <a href="https://www.google.com">Impex Videos </a>
+
+
+</html>
+
+?>
---
+++ admin/about/vid/impvideos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body style="text-alighn: center;">
+     <h1 style="color: green;">
+           IMPEX KAKAO SOLUTIONS
+          </h1>
+
+    <h2>
+  VIDEOS SECTIONS
+    </h2>
+    <p>Cocoa</p>
+    <video width = "500" height="1000px"
+        controls = "controls"/>
+        <source src="cocoa1.mp4"
+           type="video/mp4">
+    </video>
+    <br/>
+    <h3> VIDEO NUMBER TWO </h3>
+    <p> video 2 </p>
+
+    <video width = "500px" height="500px"
+        controls = "controls"/>
+        <source src="cocoa2.mp4"
+           type="video/mp4">
+    </video>
+        <br/>
+    <img src = "page0001.jpg" alt="page001"
+        style="width: 500px;height:500px;">
+
+
+</html>
